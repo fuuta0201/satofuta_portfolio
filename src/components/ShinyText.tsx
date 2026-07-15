3960a32c40ae480c02c208e552364ceeff15e342
fuuta0201 <satofuta0201@gmail.com>
2026-07-15 21:15:46 +0900

    chore: init linter, formatter

--- src/components/ShinyText.tsx
+++ src/components/ShinyText.tsx
@@ -105,7 +105,7 @@ const ShinyText: React.FC<ShinyTextProps> = ({
   // Transform: p=0 -> 150% (shine off right), p=100 -> -50% (shine off left)
   const backgroundPosition = useTransform(
     progress,
-    (p) => `${150 - p * 2}% center`,
+    (p) => `${150 - p * 2}% center`
   );
 
   const handleMouseEnter = useCallback(() => {
